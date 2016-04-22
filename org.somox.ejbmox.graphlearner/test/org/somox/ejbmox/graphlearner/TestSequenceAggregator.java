4ea1b08fc583cfea7eb048bb0409f83f87803c51
merkle <merkle@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-04-22 17:24:58 +0000

    clean up
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Addons/EJBmox@31835 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.ejbmox.graphlearner/test/org/somox/ejbmox/graphlearner/TestSequenceAggregator.java
+++ org.somox.ejbmox.graphlearner/test/org/somox/ejbmox/graphlearner/TestSequenceAggregator.java
@@ -5,9 +5,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.somox.ejbmox.graphlearner.SPGraph;
-import org.somox.ejbmox.graphlearner.GraphLearner;
-import org.somox.ejbmox.graphlearner.Path;
 import org.somox.ejbmox.graphlearner.node.LeafNode;
 
 public class TestSequenceAggregator {
@@ -49,14 +46,5 @@ public class TestSequenceAggregator {
 		
 		Assert.assertEquals("A[B|C]", learner.getGraph().toString());
 	}
-	
-	
-	
-//	@Test
-//	public void parallel_depthOne_widthTwo() {
-//		learner.addPath(Path.fromNodes(new LeafNode("A"), new LeafNode("B"), new LeafNode("C")));
-//		learner.addPath(Path.fromNodes(new LeafNode("A"), new LeafNode("B")));
-//		Assert.assertEquals("A[B|C]", learner.getGraph().toString());
-//	}
 
 }
