ccd31933892214eb3704f349fcfac4d16d2d84ff
merkle <merkle@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-05-20 14:26:58 +0000

    launch config integration
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Addons/EJBmox@32060 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.ejbmox/src/org/somox/ejbmox/ejb/ui/EJBmoxModelAnalyzerTabGroup.java
+++ org.somox.ejbmox/src/org/somox/ejbmox/ejb/ui/EJBmoxModelAnalyzerTabGroup.java
@@ -1,11 +1,14 @@
 package org.somox.ejbmox.ejb.ui;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.somox.ejbmox.util.EJBMoXWorkflowHooks;
 import org.somox.ui.runconfig.ModelAnalyzerTabGroup;
 
+import de.uka.ipd.sdq.workflow.launchconfig.extension.ExtendableTabGroup;
 import de.uka.ipd.sdq.workflow.launchconfig.tabs.DebugEnabledCommonTab;
 
 /**
@@ -16,11 +19,26 @@ import de.uka.ipd.sdq.workflow.launchconfig.tabs.DebugEnabledCommonTab;
  */
 public class EJBmoxModelAnalyzerTabGroup extends ModelAnalyzerTabGroup {
 
-    @Override
-    public void createTabs(final ILaunchConfigurationDialog dialog, final String mode) {
-        final ArrayList<ILaunchConfigurationTab> tabList = this.getCoreAnalyzerTabs();
+	@Override
+	public void createTabs(final ILaunchConfigurationDialog dialog, final String mode) {
+		final ArrayList<ILaunchConfigurationTab> tabList = this.getCoreAnalyzerTabs();
 
-        tabList.add(new DebugEnabledCommonTab());
-        this.setTabs(tabList.toArray(new ILaunchConfigurationTab[0]));
-    }
+		WorkflowExtensionsTabGroup workflowTabGroup = new WorkflowExtensionsTabGroup();
+		List<ILaunchConfigurationTab> tabs = workflowTabGroup.createExtensionTabs(dialog, mode,
+				EJBMoXWorkflowHooks.POST_SAVE_MODELS);
+		tabList.addAll(tabs);
+
+		tabList.add(new DebugEnabledCommonTab());
+		this.setTabs(tabList.toArray(new ILaunchConfigurationTab[0]));
+	}
+
+	private class WorkflowExtensionsTabGroup extends ExtendableTabGroup {
+
+		@Override
+		public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+			// nothing to do
+		}
+
+	}
+
 }
