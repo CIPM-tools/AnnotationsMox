4f5bf10ca892f5f4d1377502a8fee521a8b659dd
merkle <merkle@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-11-03 10:13:36 +0000

    move aggregation strategies to new *.aggregation package
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Addons/EJBmox@33803 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.somox.ejbmox.inspectit2pcm/src/org/somox/ejbmox/inspectit2pcm/aggregation/MeanAggregationStrategy.java
@@ -0,0 +1,25 @@
+package org.somox.ejbmox.inspectit2pcm.aggregation;
+
+import java.util.Collection;
+
+import org.palladiosimulator.pcm.core.PCMRandomVariable;
+import org.somox.ejbmox.inspectit2pcm.util.PCMHelper;
+
+/**
+ * Calculates the arithmetic mean of supplied values and builds a {@link PCMRandomVariable}
+ * representing that mean.
+ * 
+ * @author Philipp Merkle
+ *
+ */
+public class MeanAggregationStrategy implements AggregationStrategy {
+
+    @Override
+    public PCMRandomVariable aggregate(Collection<Double> values) {
+        double sum = values.stream().mapToDouble(Double::doubleValue).sum();
+        double mean = sum / values.size();
+
+        return PCMHelper.createPCMRandomVariable(mean);
+    }
+
+}
